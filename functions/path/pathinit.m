da7394926fd4d22a0d04d5b8c157adcb4e624ae8
nfagan <nick@fagan.org>
2016-09-15 18:33:21 -0400

    added ability to specify which path to initialize

--- functions/path/pathinit.m
+++ functions/path/pathinit.m
@@ -1,11 +1,15 @@
-function pathinit()
+function pathinit(name)
+
+if nargin < 1
+    name = 'hannah';
+end
 
 paths = globalpath();
 
-initial = pathfor('hannah',paths);
+initial = pathfor(name,paths);
 
 addpath(fullfile(initial,'paths'));
 
-pathchange('hannah');
+pathchange(name);
 
 end
